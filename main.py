f074f926c678aa96763e3d33fd3ff87ad3417242
jpark8215 <50881191+jpark8215@users.noreply.github.com>
2023-03-27 19:49:20 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
 import numpy as np
 import pandas as pd
+import nltk
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
 
 # import data
 data = pd.read_csv('kdrama.csv')
 # print(data)
 
+# create a set of stop words to remove from the synopsis
+stop_words = set(stopwords.words('english'))
 
 def suggest_shows(genre, cast):
     shows = data[["Name",
@@ -16,7 +21,7 @@ def suggest_shows(genre, cast):
                  # "Duration",
                  # "Content Rating",
                  # "Rating",
-                 # "Synopsis",
+                 "Synopsis",
                  "Genre",
                  # "Tags",
                  # "Director",
@@ -26,17 +31,38 @@ def suggest_shows(genre, cast):
                 # "Rank"
                 ]]
     show_arr = np.array(shows)
+    
+    # extract keywords from the synopses of previous shows
+    keywords = set()
+    for show in show_arr:
+        if show[2] != genre and all(actor not in show[3] for actor in cast):
+            synopsis = show[1].lower()
+            tokens = word_tokenize(synopsis)
+            keywords.update([token for token in tokens if token not in stop_words])
 
-    # The resulting list of suggestions is converted to a set to remove duplicates and returned.
+#     # The resulting list of suggestions is converted to a set to remove duplicates and returned.
+#     suggest = []
+#     for show in show_arr:
+#         if show[1] == genre:
+#             suggest.append(show[0])
+
+#         for actor in cast:
+#             if actor in show[2]:
+#                 suggest.append(show[0])
+#     return list(set(suggest))
+
+     # suggest new shows based on the extracted keywords
     suggest = []
     for show in show_arr:
-        if show[1] == genre:
-            suggest.append(show[0])
-
-        for actor in cast:
-            if actor in show[2]:
+        if show[2] == genre or any(actor in show[3] for actor in cast):
+            synopsis = show[1].lower()
+            tokens = word_tokenize(synopsis)
+            if any(token in keywords for token in tokens):
                 suggest.append(show[0])
+
+    # remove duplicates and return the list of suggestions
     return list(set(suggest))
+
 
 
 # Example usage
